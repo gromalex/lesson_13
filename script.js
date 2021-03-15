c319d240d47189d4898c1a2fdcf5b426190d2e7c
Alexey Gulo <alexeygulo@gmail.com>
2021-03-15 20:27:13 +0300

    Create index.html, script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// Git -----------------------------------------------------------------------
+//
+// 1. Установка GitBash (https://git-scm.com/downloads)
+//  - Соглашаемся с лицензией
+//  - Оставляем дефолтные компоненты
+//  - Выбрать редактор
+//  - https://drive.google.com/drive/folders/1Y7QhIo29MD-2AzblDWjODifek53naa2G?usp=sharing
+
+// Основные команды bash https://kmb.cybber.ru/bash/main.html
+//  $ cd - сокращенное change directory. Позволяет перемещаться по файловой системе
+//  $ ls - сокращение от list. Отображает все файлы и директории
+//  $ mkdir - сокращение от make directory. Создает директории с именем directory
+//  $ touch <file_name> - создать файл
+
+// Регистрация на https://github.com/
+
+// Основные команды git
+// $ git init - инициализация git репозитория
+// $ git status - проверить состояние файлов
+
+// $ git add . - проиндексировать текущее состояния/новые изменения, . - это все файлы с изменениями в репозитории
+// $ git commit -m "Name commit" - коммит изменений
+
+// $ git branch name_branch - создать ветку с именем name_branch
+// $ git checkout name_branch - перейти в ветку name_branch
+// $ git checkout -b name_branch - создать ветку name_branch и сразу перейти в неё
+// $ git checkout name_file - отменить изменения в файле name_file
+
+// Чтобы слить ветку name_branch с основной веткой master, надо:
+// 1. Закомитить изменения в ветке name_branch
+// 2. Перейти в ветку master $ git checkout master
+// 3. Слить ветку name_branch с master $ git merge name_branch
+
+// .gitignore
+
+// Установка node.js -----------------------------------------------------------
+// Скачать https://nodejs.org/ru/ и установить
